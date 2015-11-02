e1441cd495e63ef6e8142371186e8e33077b4fc1
The-Darkling <flameian@mac.com>
2015-11-02 04:00:27 -0500

    Added modularity with music playing, sound effect implementation
    Call the playSFX(fileName) function whenever you want to play a sound
    effect, just make sure the put in a valid sound file or else it won’t
    work. Currently takes Test, an initial sound file made for test
    purposes, but that could be updated in the future.

--- JS_v0/js/soundScripts.js
+++ JS_v0/js/soundScripts.js
@@ -1,27 +1,33 @@
 var rand = new Ziggurat();
 
-var curClip = 0;
+var curClip = [0, 0];
 
 var curStart = Date.now();
 
 var path = '../Sounds/';
 
-var tracks = 'Draft2/';
+var tracks = ['SFX/', 'Draft2/'];
+
+var playingTracks = [];
+playingTracks[0] = true;
+playingTracks[1] = true;
 
 var extension = '.ogg';
 
 var loopTrack = new Audio();
-loopTrack.src = path + tracks + 'MainLoop' + extension;
+loopTrack.src = path + tracks[1] + 'MainLoop' + extension;
 loopTrack.load;
 loopTrack.loop = true;
-loopTrack.volume = 0.5
+loopTrack.volume = 0.25;
 loopTrack.play();
 
 var audio = new Audio();
-audio.src = path + tracks + 'Clip' + curClip + extension;
+audio.src = path + tracks[1] + 'Clip' + curClip[1] + extension;
 audio.load;
-audio.volume = 1.0;
+audio.volume = 1;
 audio.play();
+
+var sfx = new Audio();
 
 //Make the entire thing one audio file, fast forward to the appropriate clips.
 var clipTimes = [0, 5334, 13335, 18669, 24003, 32004, 37338, 58674, 61341, 66675];
@@ -34,34 +40,34 @@ function detNext(prevClip)
 	var finalClip;
 	var randVal = Math.round(rand.nextGaussian());
 	
-	if(prevClip == 0 || prevClip == 3 || prevClip == 6)
+	if(prevClip === 0 || prevClip === 3 || prevClip === 6)
 	{
 		if(randVal <= -3)
 		{
 			finalClip = 1;
 		}
 		
-		if(randVal == -2)
+		if(randVal === -2)
 		{
 			finalClip = 9;
 		}
 		
-		if(randVal == -1)
+		if(randVal === -1)
 		{
 			finalClip = 2;
 		}
 		
-		if(randVal == 0)
+		if(randVal === 0)
 		{
 			finalClip = 4;
 		}
 		
-		if(randVal == 1)
+		if(randVal === 1)
 		{
 			finalClip = 8;
 		}
 		
-		if(randVal == 2)
+		if(randVal === 2)
 		{
 			finalClip = 5;
 		}
@@ -72,34 +78,34 @@ function detNext(prevClip)
 		}
 	}
 	
-	if(prevClip == 1 || prevClip == 4 || prevClip == 7)
+	if(prevClip === 1 || prevClip === 4 || prevClip === 7)
 	{
 		if(randVal <= -3)
 		{
 			finalClip = 0;
 		}
 		
-		if(randVal == -2)
+		if(randVal === -2)
 		{
 			finalClip = 9;
 		}
 		
-		if(randVal == -1)
+		if(randVal === -1)
 		{
 			finalClip = 2;
 		}
 		
-		if(randVal == 0)
+		if(randVal === 0)
 		{
 			finalClip = 6;
 		}
 		
-		if(randVal == 1)
+		if(randVal === 1)
 		{
 			finalClip = 8;
 		}
 		
-		if(randVal == 2)
+		if(randVal === 2)
 		{
 			finalClip = 5;
 		}
@@ -110,34 +116,34 @@ function detNext(prevClip)
 		}
 	}
 	
-	if(prevClip == 2 || prevClip == 5 || prevClip == 8)
+	if(prevClip === 2 || prevClip === 5 || prevClip === 8)
 	{
 		if(randVal <= -3)
 		{
 			finalClip = 0;
 		}
 		
-		if(randVal == -2)
+		if(randVal === -2)
 		{
 			finalClip = 9;
 		}
 		
-		if(randVal == -1)
+		if(randVal === -1)
 		{
 			finalClip = 7;
 		}
 		
-		if(randVal == 0)
+		if(randVal === 0)
 		{
 			finalClip = 6;
 		}
 		
-		if(randVal == 1)
+		if(randVal === 1)
 		{
 			finalClip = 4;
 		}
 		
-		if(randVal == 2)
+		if(randVal === 2)
 		{
 			finalClip = 1;
 		}
@@ -156,27 +162,27 @@ function detNext(prevClip)
 			finalClip = 0;
 		}
 		
-		if(randVal == -2)
+		if(randVal === -2)
 		{
 			finalClip = 9;
 		}
 		
-		if(randVal == -1)
+		if(randVal === -1)
 		{
 			finalClip = 1;
 		}
 		
-		if(randVal == 0)
+		if(randVal === 0)
 		{
 			finalClip = 8;
 		}
 		
-		if(randVal == 1)
+		if(randVal === 1)
 		{
 			finalClip = 2;
 		}
 		
-		if(randVal == 2)
+		if(randVal === 2)
 		{
 			finalClip = 7;
 		}
@@ -191,16 +197,47 @@ function detNext(prevClip)
 	return finalClip;
 }
 
-function changeSound()
+function changeClip()
 {
+    if(playingTracks[1])
+    {
 	if(Date.now() - curStart >= audio.duration*1000)
 	{
-		curClip = detNext(curClip);
-		audio.src = path + tracks + 'Clip' + curClip + extension;
-		curStart = Date.now();
-		audio.load;
-		audio.play();
+            playSFX('Test');
+            curClip[1] = detNext(curClip[1]);
+            audio.src = path + tracks[1] + 'Clip' + curClip[1] + extension;
+            curStart = Date.now();
+            audio.load;
+            audio.play();
 	}
+    }
 }
 
-setInterval(changeSound, 1);
+//To toggle a track, just enter in its name to match the if statements.
+function swapTrack(trackName)
+{
+    if(trackName === 'MainTheme')
+    {
+        if(playingTracks[1])
+        {
+            playingTracks[1] = false;
+        }
+        
+        else
+            if(playingTracks[1])
+        {
+            playingTracks[1] = true;
+        }
+    }
+}
+
+//Currently compatible with: Test
+function playSFX(clipName)
+{
+    sfx.src = path + tracks[0] + clipName + extension;
+    sfx.volume = 1;
+    sfx.load;
+    sfx.play();
+}
+
+setInterval(changeClip, 30);
